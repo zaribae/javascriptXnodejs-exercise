086c4c458738393c7a0ed06c5e739e95182cdbfb
Zari Bae <ahmadazzhari@gmail.com>
2022-11-11 22:27:39 +0700

    hash function added

--- secrets/app.js
+++ secrets/app.js
@@ -1,10 +1,9 @@
-//jshint esversion:6
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -19,7 +18,6 @@ const userSchema = new mongoose.Schema ({
     password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 
 const User = new mongoose.model("User", userSchema);
@@ -36,10 +34,14 @@ app.get('/register', (req, res) => {
     res.render("register");
 })
 
+app.get('/logout', (req, res) => {
+    res.render("home");
+})
+
 app.post('/register', (req, res) => {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save((err) => {
@@ -53,7 +55,7 @@ app.post('/register', (req, res) => {
 
 app.post('/login', (req, res) => {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
         User.findOne({email: username}, (err, foundUser) => {
             if (!err) {
                 if (foundUser.password === password) {
